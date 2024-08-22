802e298e03950852812385057c9a2e0050e2f03d
ih-abir <ih.abir.1@gmail.com>
2024-08-22 17:23:22 +0600

    update AboutIntro image

--- app/components/AboutIntro.tsx
+++ app/components/AboutIntro.tsx
@@ -31,9 +31,6 @@ const AboutIntro = async () => {
             <Image 
               className="w-full h-full" 
               src={HomeAboutBg}
-              width={640}
-              height={705.08}
-              sizes="(max-width: 1280px) 640px, (max-width: 1024px) calc(50vw - 64px), calc(100vw - 64px)"
               alt=""
             />
           </div>
@@ -51,8 +48,8 @@ const AboutIntro = async () => {
                 className="w-full h-full object-cover"
                 src={blob?.url}
                 width={640}
-                height={705.08}
-                sizes="(max-width: 767px) calc(100vw - 64px), (max-width: 1024px) calc(50vw - 64px), 640px"
+                height={705}
+                sizes="(min-width: 1280px) 640px, (min-width: 768px) 320px, calc(100vw-64px)"
                 alt={blob.alternativeText}
               />
             </div>
