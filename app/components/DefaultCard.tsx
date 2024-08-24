8fee607156e2539f2b1f89605cd557220b9e2fe9
ih-abir <ih.abir.1@gmail.com>
2024-08-24 10:39:47 +0600

    add default card component

---
+++ app/components/DefaultCard.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import Markdown from "react-markdown";
+
+import PlaybtnIcon from "@images/playbtn.svg";
+import styles from '@styles/default-card.module.scss';
+
+type Props = {
+  title: string;
+  slug: string;
+  blob: {
+    url: string;
+    width: number;
+    height: number;
+    alternativeText?: string;
+  };
+  intro_text: string;
+  googleData: {
+    rating: number;
+    user_ratings_total: number;
+  };
+  Book_link?: string;
+};
+
+export default function Card({
+  Title: title,
+  Description: description,
+  Intro_blob: {
+    data: { attributes: blob },
+  },
+  google_place_id,
+  slug,
+  Book_link,
+  googleMapsData,
+}: Props) {
+  const [intro_text] = description.split("\n");
+
+  return (
+    <div
+      className={`${styles.defaultCard} bg-white overflow-hidden rounded-[24px]`}
+      role="listitem"
+      aria-label={title}
+    >
+      <div className={`${styles.defaultCardImgContainer} overflow-hidden`}>
+        <a href={`/${slug}/`} className="cursor-pointer">
+          <div className="w-full h-full">
+            <Image
+              className="w-full h-full object-cover"
+              src={blob?.url}
+              alt={blob?.alternativeText}
+              width={280}
+              height={280}
+            />
+          </div>
+        </a>
+      </div>
+      <div className="px-4 pt-4 pb-5 lg:pb-8">
+        <div className="mb-2 text-xl font-bold text-greenpea">
+          <a href={`/${slug}/`} className="cursor-pointer">
+            {title}
+          </a>
+        </div>
+        <div className="prose mb-2 leading-[120%] line-clamp-3">
+          <Markdown>{intro_text}</Markdown>
+        </div>
+        {/*<div className="flex items-center gap-1 md:gap-2.5">
+          <div className="stars-outer flex my-auto text-sm sm:text-base">
+            <div
+              className="stars-inner flex text-sm sm:text-base"
+              style={{ width: `${googleData?.rating * 20}%` }}
+            />
+          </div>
+          <div className="flex items-center gap-1.5">
+            <div className="sm:text-[17px] font-semibold">{googleData?.rating}</div>
+            <div className="text-[13px] font-medium">({googleData?.user_ratings_total})</div>
+          </div>
+        </div>*/}
+        {Book_link && (
+          <div className="flex items-center mt-5">
+            <a
+              href={`/${slug}/`}
+              className={[
+                "bg-downy flex items-center px-5 py-2 roboto",
+                "text-xs lg:text-sm font-medium cursor-pointer rounded-full"
+              ].join(' ')}
+            >
+              Discover
+              <div className="h-3.5 ml-2.5">
+                <Image className="w-full h-full" src={PlaybtnIcon} alt="" />
+              </div>
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
