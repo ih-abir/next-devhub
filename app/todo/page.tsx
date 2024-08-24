aeee977c20a33da1dcf0e94c31245ca0c157cbe0
ih-abir <ih.abir.1@gmail.com>
2024-08-24 10:44:08 +0600

    update Home todo with defaultCrad component

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import Markdown from "react-markdown";
+
 import Hero from "@components/Hero";
 import AboutIntro from "@components/AboutIntro";
+import DefaultCard from "@components/DefaultCard";
 
 import CMS from "@utils/CMS";
 
@@ -12,13 +14,22 @@ const HomeTodo = async () => {
     Intro_text,
     Intro_blob,
   } = await CMS.get('homeTodo'),
-    todos = await CMS.get('todos');
+    posts = await CMS.get('todos');
 
   return (
     <>
       <Hero Title={Title} Intro_text={Intro_text} Intro_blob={Intro_blob} />
 
-
+      <div class="wrapper overflow-x-hidden px-8 xl:px-3 py-36 mx-auto">
+        <div
+          role="list"
+          className="grid grid-cols-[repeat(auto-fit,minmax(0,280px))] lg:grid-cols-3 gap-[35px] justify-center"
+        >
+          {posts.map((post, index) => (
+            <DefaultCard key={index} {...post} />
+          ))}
+        </div>
+      </div>
 
       <AboutIntro />
     </>
