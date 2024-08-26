56c443be2f22fbb7875742e680573998b8a34a97
ih-abir <ih.abir.1@gmail.com>
2024-08-26 11:01:54 +0600

    add dynamic todo postDetails generation

---
+++ app/todo/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from "next/navigation";
+import PostDetails from "@components/PostDetails";
+import CMS from "@utils/CMS";
+
+const todos = await CMS.get("todos");
+
+export async function generateStaticParams() {
+  return todos.map(({ Meta: { URL_slug } }) => ({ slug: URL_slug }));
+}
+
+export default function AccommodationPage({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const todo =
+    todos.find(({ Meta: { URL_slug } }) => URL_slug === slug) ??
+    notFound();
+
+  return <PostDetails {...todo} />;
+}
