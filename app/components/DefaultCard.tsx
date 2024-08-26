ed1c2a097d14eed01174a1f958c450b25df9cd50
ih-abir <ih.abir.1@gmail.com>
2024-08-26 11:26:16 +0600

    update slug of DefaultCards

--- app/components/DefaultCard.tsx
+++ app/components/DefaultCard.tsx
@@ -11,6 +11,7 @@ interface BlobAttributes {
 }
 
 interface DefaultCardProps {
+  type: string;
   Title: string;
   Description: string;
   Intro_blob: {data : {attributes: BlobAttributes}};
@@ -22,15 +23,17 @@ interface DefaultCardProps {
 const DefaultCard = async ( props: DefaultCardProps ) => {
 
   const {
+    type,
     Title: title,
     Description: description,
     Intro_blob: { data: { attributes: blob } },
     Book_link,
     google_place_id,
-    Meta: { URL_slug: slug }
+    Meta: { URL_slug }
   } = props;
 
-  const [intro_text] = description.split("\n");
+  const [intro_text] = description.split("\n"),
+     slug = `/${type === "todoDetails" ? "todo" : "accommodation"}/${URL_slug}/`;
 
   return (
     <div
@@ -39,7 +42,7 @@ const DefaultCard = async ( props: DefaultCardProps ) => {
       className={`${styles.defaultCard} bg-white overflow-hidden rounded-[24px]`}
     >
       <div className={`${styles.defaultCardImgContainer} overflow-hidden`}>
-        <Link href={`/${slug}/`} className="cursor-pointer">
+        <Link href={slug} className="cursor-pointer">
           <div className="w-full h-full">
             <Image
               className="w-full h-full object-cover"
@@ -57,7 +60,7 @@ const DefaultCard = async ( props: DefaultCardProps ) => {
       </div>
       <div className="px-4 pt-4 pb-5 lg:pb-8">
         <div className="mb-2 text-xl font-bold text-greenpea">
-          <Link href={`/${slug}/`} className="cursor-pointer">
+          <Link href={slug} className="cursor-pointer">
             {title}
           </Link>
         </div>
@@ -79,7 +82,7 @@ const DefaultCard = async ( props: DefaultCardProps ) => {
         {Book_link && (
           <div className="flex items-center mt-5">
             <Link
-              href={`/${slug}/`}
+              href={slug}
               className={[
                 "bg-downy flex items-center px-5 py-2 roboto",
                 "text-xs lg:text-sm font-medium cursor-pointer rounded-full"
