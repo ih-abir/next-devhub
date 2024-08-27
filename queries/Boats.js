eabe5b0764a45555693434a873a9fddeb5f6d537
ih-abir <ih.abir.1@gmail.com>
2024-08-27 13:15:15 +0600

    update Boats query for Departure

--- queries/Boats.js
+++ queries/Boats.js
@@ -2,7 +2,7 @@ import Image from "./components/Image.js";
 
 const BoatsQuery = `
   {
-    boats( pagination: { limit: -1 }) {
+    boats(pagination: { limit: -1 }) {
       data{
         attributes{
           Title
@@ -12,6 +12,21 @@ const BoatsQuery = `
           Book_link
           Logo ${Image}
           Boat_image ${Image}
+          Departure{
+            Starting_point_name
+            Ending_point_name
+            Starting_point_google_place_id
+            Ending_point_google_place_id
+            IDR_price_one_way_adult
+            IDR_price_return_adult
+            IDR_price_one_way_child
+            IDR_price_return_child
+            Price_child_age_range
+            Time{
+              Departure_time
+              Return_time
+            }
+          }
           createdAt
         }
       }
