1d25ae5ae7c19c220f2afb5aeabf1d40be84096c
ih-abir <ih.abir.1@gmail.com>
2024-08-31 10:36:38 +0600

    add pages/Posts to generate dynamic todo, accommodation page by new slug

---
+++ src/app/pages/Posts.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+import Markdown from "react-markdown";
+
+import Hero from "@components/Hero";
+import AboutIntro from "@components/AboutIntro";
+import DefaultCard from "@components/DefaultCard";
+
+const HomeTodo = async (props) => {
+  const {
+    type,
+    Title,
+    Intro_text,
+    Intro_blob,
+    posts,
+  } = props.page;
+
+  return (
+    <>
+      <Hero Title={Title} Intro_text={Intro_text} Intro_blob={Intro_blob} />
+
+      <div className="wrapper overflow-x-hidden px-8 xl:px-3 py-36 mx-auto">
+        <div
+          role="list"
+          className="grid grid-cols-[repeat(auto-fit,minmax(0,280px))] lg:grid-cols-3 gap-[35px] justify-center"
+        >
+          {posts.map((post: any, type: string, index: number) => (
+            <DefaultCard key={index} type={type} {...post} />
+          ))}
+        </div>
+      </div>
+
+      <AboutIntro />
+    </>
+  )
+}
+
+export default HomeTodo;
