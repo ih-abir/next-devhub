ad27ef6d7cd79429f19d1f99accde189e60a8bf9
ih-abir <ih.abir.1@gmail.com>
2024-08-31 20:46:51 +0600

    update BasicPage logic

--- src/app/pages/BasicPage.tsx
+++ src/app/pages/BasicPage.tsx
@@ -36,47 +36,51 @@ const BasicPage = async (props: BasicPageProps) => {
     <>
       <Hero Title={Title} Intro_text={Intro_text} Intro_blob={Intro_blob} />
       
-      <div className="py-[75px]">
-        {block_blob && (
-          <div className="wrapper mx-auto px-8 xl:px-3 overflow-x-hidden">
-            <div
-              role="banner"
-              className={[
-                styles.detailsBannerImgContainer,
-                "overflow-hidden w-full"
-              ].join(' ')}
-            >
-              <Image
-                className="w-full h-full object-cover"
-                src={block_blob.url}
-                alt={block_blob.alternativeText}
-                width={1230}
-                height={692}
-                sizes="(min-width: 1280px) 1230px, calc(100vw - 64px)"
-                placeholder="blur"
-                blurDataURL={await ImageBase64(block_blob.url)}
-              />
+      { 
+        (block_blob || block_title || block_text) && (
+          <div className="py-[75px]">
+            {block_blob && (
+              <div className="wrapper mx-auto px-8 xl:px-3 overflow-x-hidden">
+                <div
+                  role="banner"
+                  className={[
+                    styles.detailsBannerImgContainer,
+                    "overflow-hidden w-full"
+                  ].join(' ')}
+                >
+                  <Image
+                    className="w-full h-full object-cover"
+                    src={block_blob.url}
+                    alt={block_blob.alternativeText}
+                    width={1230}
+                    height={692}
+                    sizes="(min-width: 1280px) 1230px, calc(100vw - 64px)"
+                    placeholder="blur"
+                    blurDataURL={await ImageBase64(block_blob.url)}
+                  />
+                </div>
+              </div>
+            )}
+
+            <div className="wrapper overflow-x-hidden mx-auto px-8 xl:px-3 pt-[75px]">
+              {block_title && (
+                <h2
+                  className={[
+                    block_text && "mb-10",
+                    "text-[clamp(2.125rem,4vw+.3rem,3rem)] font-semibold",
+                  ].join(' ')}
+                >
+                  {block_title}
+                </h2>
+              )}
+
+              <div className="text-xl prose">
+                <Markdown>{block_text}</Markdown>
+              </div>
             </div>
           </div>
-        )}
-
-        <div className="wrapper overflow-x-hidden mx-auto px-8 xl:px-3 pt-[75px]">
-          {block_title && (
-            <h2
-              className={[
-                block_text && "mb-10",
-                "text-[clamp(2.125rem,4vw+.3rem,3rem)] font-semibold",
-              ].join(' ')}
-            >
-              {block_title}
-            </h2>
-          )}
-
-          <div className="text-xl prose">
-            <Markdown>{block_text}</Markdown>
-          </div>
-        </div>
-      </div>
+        )
+      }
     </>
   );
 }
